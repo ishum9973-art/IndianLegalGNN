1f2f4d57c4640cbc927b8253e8134736faccfea8
ishum9973-art <ishum9973@gmail.com>
2026-03-13 11:10:21 +0530

    update code

--- PromptCase2017_embeddings.sh
+++ PromptCase2017_embeddings.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # generate PromptCase embeddings for COLIEE 2017 train and test
-python PromptCase/promptcase_embedding/PromptCase_embedding_generation.py --data 2017 --dataset test
-python PromptCase/promptcase_embedding/PromptCase_embedding_generation.py --data 2017 --dataset train
+python PromptCase/PromptCase_embedding_generation.py --data 2017 --dataset test
+python PromptCase/PromptCase_embedding_generation.py --data 2017 --dataset train
